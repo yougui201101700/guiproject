01d75dab1b1014137f7ace28f594691b0fa77144
ygt <yougui201101700@163.com>
2017-11-06 15:37:17 +0800

    第四次提交

--- test01/src/test01/demo01.java
+++ test01/src/test01/demo01.java
@@ -11,10 +11,27 @@ package test01;
  *	@Date: 2017年11月6日
  */
 public class demo01 {
+	
+	
+	
+			private String name;
+			
+			public void setName(String name){
+				this.name = name;
+			}
+			public String getName(){
+				return name;
+			}
 		public static void main(String[] args) {
+			
 			if(i = 0,i<100,i++){
 				System.out.println("hello world git！！！！,看到你的世界");
 			}
 			
+			
+			
 		}
+		
+		
+		
 }
